933db64001cbf8dd314cf4453c60a7b2ee493004
Nshan Bagdasarian <nshan.b@outlook.com>
2018-07-03 17:09:31 -0700

    Added routing global vars

--- routes/main.js
+++ routes/main.js
@@ -3,32 +3,40 @@ const router = require('express').Router();
 const request = require('request');
 const fs = require('fs');
 
-// API
-var BITTREX = 'https://bittrex.com/api/v1.1/public/';
 
+var ABOUT = '/about';
+var BITTREX = 'https://bittrex.com/api/v1.1/public/';
+var CONTACT = '/contact';
+var GRAPH = '/g/:market.:left.:right';
+var GRAPHS = '/graphs';
+var MARKETS = '/markets';
+var MARKETS_M = MARKETS + '/:market';
+var MARKETSUMMARIES = '/marketsummaries';
+
+// API
 router.get('/', (req, res) => {
     res.render('table');
 });
 
-router.get('/about', (req, res) => {
+router.get(ABOUT, (req, res) => {
     res.render('about');
 });
 
-router.get('/contact', (req, res) => {
+router.get(CONTACT, (req, res) => {
     res.render('contact');
 });
 
-router.get('/graphs', (req,res) => {
+router.get(GRAPHS, (req,res) => {
     res.render('graphs.ejs');
 });
 
-router.get('/g/:market.:left.:right', (req, res) => {
+router.get(GRAPH, (req, res) => {
     //res.send(req.params)
     res.render('graph.ejs', req.params);
 });
 
 // Get available markets
-router.get('/markets', (req, res) => {
+router.get(MARKETS, (req, res) => {
     var url = BITTREX + 'getmarkets';
     request(url, function(error, response, body) {
         var result = JSON.parse(JSON.stringify(eval('(' + body + ')')));
@@ -37,7 +45,7 @@ router.get('/markets', (req, res) => {
 });
 
 // Get market summaries
-router.get('/marketsummaries', (req, res) => {
+router.get(MARKETSUMMARIES, (req, res) => {
     var url = BITTREX + 'getmarketsummaries';
     request(url, function(error, response, body) {
         var result = JSON.parse(JSON.stringify(eval('(' + body + ')')));
@@ -46,7 +54,7 @@ router.get('/marketsummaries', (req, res) => {
 });
 
 // Get current tick value
-router.get('/markets/:market', (req, res) => {
+router.get(MARKETS_M, (req, res) => {
     var market = req.params.market.trim();
     var url = BITTREX + 'getticker?market=' + market;
     request(url, function(error, response, body) {
